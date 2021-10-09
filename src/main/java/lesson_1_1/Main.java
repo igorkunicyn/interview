36c2f3391ad7537d3c3752803f6376f78abc547f
Igor Kunicyn <igor_kunicyn@inbox.ru>
2021-10-09 17:04:27 +0300

    Initial commit

---
+++ src/main/java/lesson_1_1/Main.java
@@ -0,0 +1,17 @@
+package lesson_1_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person.Builder()
+                .addFirstName("Igor")
+                .addMiddleName("Aleksandrovich")
+                .addLastName("Kunisyn")
+                .addCountry("Russian")
+                .addAddress("Penza")
+                .addGender("Man")
+                .addPhone("561212")
+                .addAge(45)
+                .build();
+        System.out.println(person);
+    }
+}
